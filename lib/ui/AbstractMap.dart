07f3542fe6a86787f7dbc9ede67456213a88dc10
jean-paul Jacquot <jacquot.jpaul@gmail.com>
2022-11-15 11:35:38 +0100

    add map

--- lib/ui/AbstractMap.dart
+++ lib/ui/AbstractMap.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'Maps.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 abstract class MapWidget extends StatefulWidget {
-  factory MapWidget() => getMapWidget();
+  factory MapWidget() => getMap();
+}
+
+Widget getMap() {
+  return kIsWeb ? getMapWidget() : getMapWidget();
 }
