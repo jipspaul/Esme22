3e85ab4036fe9d6419b7d36d1281745942a53d07
jean-paul Jacquot <jacquot.jpaul@gmail.com>
2022-11-15 08:55:27 +0100

    wip

---
+++ lib/domain/QuestionManager.dart
@@ -0,0 +1,37 @@
+import 'package:flutter_application_1/data/models/Question.dart';
+import 'package:flutter_application_1/data/repositories/IQuestionRepo.dart';
+import 'package:flutter_application_1/data/repositories/MockQuestionRepo.dart';
+import 'package:flutter_application_1/domain/QuestionUseCase.dart';
+
+class QuestionManager {
+  List<Question> _questions;
+  int _index; //current question index
+  int _score;
+
+  QuestionManager() {
+    IQuestionRepo questionRepo = MockQuestionRepo();
+
+    _questions = questionRepo.getQuestion();
+    _index = 0;
+    _score = 0;
+  }
+
+  Question getCurrentQuestion() {
+    return _questions[_index];
+  }
+
+  bool submitResponse(String response) {
+    bool isCorrect =
+        QuestionUseCase().isCorrectAnswer(getCurrentQuestion(), response);
+
+    if (isCorrect) {
+      _score++;
+      if (_index < _questions.length - 1) {
+        _index++;
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
